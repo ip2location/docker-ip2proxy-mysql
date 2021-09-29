97ca728c6b12f3af14342e2a6c0957c8b971e41f
IP2Location <support@ip2location.com>
2021-09-29 16:17:12 +0800

    Added supports to query IPv4 and IPv6 database on same time.

--- run.sh
+++ run.sh
@@ -3,7 +3,10 @@
 error() { echo -e "\e[91m$1\e[m"; exit 0; }
 success() { echo -e "\e[92m$1\e[m"; }
 
-if [ -f /config ]; then
+USER_AGENT="Mozilla/5.0+(compatible; IP2Proxy/MySQL-Docker; https://hub.docker.com/r/ip2proxy/mysql)"
+CODES=("PX1-LITE PX2-LITE PX3-LITE PX4-LITE PX5-LITE PX6-LITE PX7-LITE PX8-LITE PX9-LITE PX10-LITE PX11-LITE PX1 PX2 PX3 PX4 PX5 PX6 PX7 PX8 PX9 PX10 PX11")
+
+if [ -f /ip2proxy.conf ]; then
 	/etc/init.d/mysql restart >/dev/null 2>&1
 	tail -f /dev/null
 fi
@@ -13,16 +16,25 @@ if [ "$TOKEN" == "FALSE" ]; then
 fi
 
 if [ "$CODE" == "FALSE" ]; then
-	error "Missing product code."
+	error "Missing database code."
 fi
 
 if [ "$MYSQL_PASSWORD" == "FALSE" ]; then
-	error "Missing MySQL password."
+	MYSQL_PASSWORD="$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-12})"
 fi
 
-if [ -z "$(echo $CODE | grep 'PX')" ]; then
+FOUND=""
+for i in "${CODES[@]}"; do
+	if [ "$i" == "$CODE" ] ; then
+		FOUND="$CODE"
+	fi
+done
+
+if [ -z $FOUND == "" ]; then
 	error "Download code is invalid."
 fi
+
+CODE=$(echo $CODE | sed 's/-//')
 
 echo -n " > Create directory /_tmp "
 
@@ -34,27 +46,58 @@ cd /_tmp
 
 echo -n " > Download IP2Proxy database "
 
-wget -O database.zip -q --user-agent="Docker-IP2Proxy/MySQL" http://www.ip2location.com/download?token=${TOKEN}\&productcode=${CODE} 2>&1
+if [ "$IP_TYPE" == "IPV4" ]; then
+	wget -O ipv4.zip -q --user-agent="$USER_AGENT" "https://www.ip2location.com/download?token=${TOKEN}&code=${CODE}CSV" > /dev/null 2>&1
 
-[ ! -f database.zip ] && error "[ERROR]"
+	[ ! -z "$(grep 'NO PERMISSION' database.zip)" ] && error "[DENIED]"
+	[ ! -z "$(grep '5 TIMES' database.zip)" ] && error "[QUOTA EXCEEDED]"
 
-[ ! -z "$(grep 'NO PERMISSION' database.zip)" ] && error "[PERMISSION DENIED]"
+	RESULT=$(unzip -t ipv4.zip >/dev/null 2>&1)
 
-[ ! -z "$(grep '5 TIMES' database.zip)" ] && error "[QUOTA EXCEEDED]"
+	[ $? -ne 0 ] && error "[FILE CORRUPTED]"
+elif [ "$IP_TYPE" == "IPV6" ]; then
+	wget -O ipv6.zip -q --user-agent="$USER_AGENT" "https://www.ip2location.com/download?token=${TOKEN}&code=${CODE}CSVIPV6" > /dev/null 2>&1
 
-[ $(wc -c < database.zip) -lt 512000 ] && error "[FILE CORRUPTED]"
+	[ ! -z "$(grep 'NO PERMISSION' database.zip)" ] && error "[DENIED]"
+	[ ! -z "$(grep '5 TIMES' database.zip)" ] && error "[QUOTA EXCEEDED]"
+
+	RESULT=$(unzip -t ipv6.zip >/dev/null 2>&1)
+
+	[ $? -ne 0 ] && error "[FILE CORRUPTED]"
+else
+	wget -O ipv4.zip -q --user-agent="$USER_AGENT" "https://www.ip2location.com/download?token=${TOKEN}&code=${CODE}CSV" > /dev/null 2>&1
+	wget -O ipv6.zip -q --user-agent="$USER_AGENT" "https://www.ip2location.com/download?token=${TOKEN}&code=${CODE}CSVIPV6" > /dev/null 2>&1
+
+	[ ! -z "$(grep 'NO PERMISSION' ipv4.zip)" ] && error "[DENIED]"
+	[ ! -z "$(grep '5 TIMES' ipv4.zip)" ] && error "[QUOTA EXCEEDED]"
+
+	[ ! -z "$(grep 'NO PERMISSION' ipv6.zip)" ] && error "[DENIED]"
+	[ ! -z "$(grep '5 TIMES' ipv6.zip)" ] && error "[QUOTA EXCEEDED]"
+
+	RESULT=$(unzip -t ipv4.zip >/dev/null 2>&1)
+	[ $? -ne 0 ] && error "[FILE CORRUPTED]"
+
+	RESULT=$(unzip -t ipv6.zip >/dev/null 2>&1)
+	[ $? -ne 0 ] && error "[FILE CORRUPTED]"
+fi
 
 success "[OK]"
 
-echo -n " > Decompress downloaded package "
+for ZIP in $(ls | grep '.zip'); do
+	CSV=$(unzip -l $ZIP | grep '.CSV' | awk '{ print $4 }')
 
-unzip -q -o database.zip
+	echo -n " > Decompress $CSV from $ZIP "
 
-CSV="$(find . -name 'IP2PROXY*.CSV')"
+	unzip -jq $ZIP $CSV
 
-[ -z "$CSV" ] && error "[ERROR]" || success "[OK]"
+	if [ ! -f $CSV ]; then
+		error "[ERROR]"
+	fi
 
-/etc/init.d/mysql start >/dev/null 2>&1
+	success "[OK]"
+done
+
+/etc/init.d/mysql start > /dev/null 2>&1
 
 echo -n " > [MySQL] Create database \"ip2proxy_database\" "
 RESPONSE="$(mysql -e 'CREATE DATABASE IF NOT EXISTS ip2proxy_database' 2>&1)"
@@ -66,64 +109,60 @@ echo -n " > [MySQL] Create table \"ip2proxy_database_tmp\" "
 RESPONSE="$(mysql ip2proxy_database -e 'DROP TABLE IF EXISTS ip2proxy_database_tmp' 2>&1)"
 
 case "$CODE" in
-	PX1|PX1IPV6|PX1LITECSV|PX1LITECSVIPV6 )
+	PX1|PX1LITE )
 		FIELDS=',`country_code` CHAR(2) NOT NULL,`country_name` VARCHAR(64) NOT NULL'
 	;;
 
-	PX2|PX2IPV6|PX2LITECSV|PX2LITECSVIPV6 )
+	PX2|PX2LITE )
 		FIELDS=',`proxy_type` VARCHAR(3) NOT NULL, `country_code` CHAR(2) NOT NULL,`country_name` VARCHAR(64) NOT NULL'
 	;;
 
-	PX3|PX3IPV6|PX3LITECSV|PX3LITECSVIPV6 )
+	PX3|PX3LITE )
 		FIELDS=',`proxy_type` VARCHAR(3) NOT NULL, `country_code` CHAR(2) NOT NULL,`country_name` VARCHAR(64) NOT NULL,`region_name` VARCHAR(128) NOT NULL,`city_name` VARCHAR(128) NOT NULL'
 	;;
 
-	PX4|PX4IPV6|PX4LITECSV|PX4LITECSVIPV6 )
+	PX4|PX4LITE )
 		FIELDS=',`proxy_type` VARCHAR(3) NOT NULL, `country_code` CHAR(2) NOT NULL,`country_name` VARCHAR(64) NOT NULL,`region_name` VARCHAR(128) NOT NULL,`city_name` VARCHAR(128) NOT NULL,`isp` VARCHAR(255) NOT NULL'
 	;;
 
-	PX5|PX5IPV6|PX5LITECSV|PX5LITECSVIPV6 )
+	PX5|PX5LITE )
 		FIELDS=',`proxy_type` VARCHAR(3) NOT NULL, `country_code` CHAR(2) NOT NULL,`country_name` VARCHAR(64) NOT NULL,`region_name` VARCHAR(128) NOT NULL,`city_name` VARCHAR(128) NOT NULL,`isp` VARCHAR(255) NOT NULL,`domain` VARCHAR(128) NOT NULL'
 	;;
 
-	PX6|PX6IPV6|PX6LITECSV|PX6LITECSVIPV6 )
+	PX6|PX6LITE )
 		FIELDS=',`proxy_type` VARCHAR(3) NOT NULL, `country_code` CHAR(2) NOT NULL,`country_name` VARCHAR(64) NOT NULL,`region_name` VARCHAR(128) NOT NULL,`city_name` VARCHAR(128) NOT NULL,`isp` VARCHAR(255) NOT NULL,`domain` VARCHAR(128) NOT NULL,`usage_type` VARCHAR(11) NOT NULL'
 	;;
 
-	PX7|PX7IPV6|PX7LITECSV|PX7LITECSVIPV6 )
+	PX7|PX7LITE )
 		FIELDS=',`proxy_type` VARCHAR(3) NOT NULL, `country_code` CHAR(2) NOT NULL,`country_name` VARCHAR(64) NOT NULL,`region_name` VARCHAR(128) NOT NULL,`city_name` VARCHAR(128) NOT NULL,`isp` VARCHAR(255) NOT NULL,`domain` VARCHAR(128) NOT NULL,`usage_type` VARCHAR(11) NOT NULL,`asn` VARCHAR(6) NOT NULL,`as` VARCHAR(256) NOT NULL'
 	;;
 
-	PX8|PX8IPV6|PX8LITECSV|PX8LITECSVIPV6 )
+	PX8|PX8LITE )
 		FIELDS=',`proxy_type` VARCHAR(3) NOT NULL, `country_code` CHAR(2) NOT NULL,`country_name` VARCHAR(64) NOT NULL,`region_name` VARCHAR(128) NOT NULL,`city_name` VARCHAR(128) NOT NULL,`isp` VARCHAR(255) NOT NULL,`domain` VARCHAR(128) NOT NULL,`usage_type` VARCHAR(11) NOT NULL,`asn` VARCHAR(6) NOT NULL,`as` VARCHAR(256) NOT NULL,`last_seen` INT(10) NOT NULL'
 	;;
 
-	PX9|PX9IPV6|PX9LITECSV|PX9LITECSVIPV6 )
+	PX9|PX9LITE )
 		FIELDS=',`proxy_type` VARCHAR(3) NOT NULL, `country_code` CHAR(2) NOT NULL,`country_name` VARCHAR(64) NOT NULL,`region_name` VARCHAR(128) NOT NULL,`city_name` VARCHAR(128) NOT NULL,`isp` VARCHAR(255) NOT NULL,`domain` VARCHAR(128) NOT NULL,`usage_type` VARCHAR(11) NOT NULL,`asn` VARCHAR(6) NOT NULL,`as` VARCHAR(256) NOT NULL,`last_seen` INT(10) NOT NULL, `threat` VARCHAR(128)'
 	;;
 
-	PX10|PX10IPV6|PX10LITECSV|PX10LITECSVIPV6 )
+	PX10|PX10LITE )
 		FIELDS=',`proxy_type` VARCHAR(3) NOT NULL, `country_code` CHAR(2) NOT NULL,`country_name` VARCHAR(64) NOT NULL,`region_name` VARCHAR(128) NOT NULL,`city_name` VARCHAR(128) NOT NULL,`isp` VARCHAR(255) NOT NULL,`domain` VARCHAR(128) NOT NULL,`usage_type` VARCHAR(11) NOT NULL,`asn` VARCHAR(6) NOT NULL,`as` VARCHAR(256) NOT NULL,`last_seen` INT(10) NOT NULL, `threat` VARCHAR(128)'
 	;;
 
-	PX11|PX11IPV6|PX11LITECSV|PX11LITECSVIPV6 )
+	PX11|PX11LITE )
 		FIELDS=',`proxy_type` VARCHAR(3) NOT NULL, `country_code` CHAR(2) NOT NULL,`country_name` VARCHAR(64) NOT NULL,`region_name` VARCHAR(128) NOT NULL,`city_name` VARCHAR(128) NOT NULL,`isp` VARCHAR(255) NOT NULL,`domain` VARCHAR(128) NOT NULL,`usage_type` VARCHAR(11) NOT NULL,`asn` VARCHAR(6) NOT NULL,`as` VARCHAR(256) NOT NULL,`last_seen` INT(10) NOT NULL, `threat` VARCHAR(128),`provider` VARCHAR(256) NOT NULL'
 	;;
 esac
 
-if [ ! -z "$(echo $CODE | grep 'IPV6')" ]; then
-	RESPONSE="$(mysql ip2proxy_database -e 'CREATE TABLE ip2proxy_database_tmp (`ip_from` DECIMAL(39,0) UNSIGNED NOT NULL,`ip_to` DECIMAL(39,0) UNSIGNED NOT NULL'"$FIELDS"',INDEX `idx_ip_to` (`ip_to`)) ENGINE=MyISAM' 2>&1)"
-else
-	RESPONSE="$(mysql ip2proxy_database -e 'CREATE TABLE ip2proxy_database_tmp (`ip_from` INT(11) UNSIGNED NOT NULL,`ip_to` INT(11) UNSIGNED NOT NULL'"$FIELDS"',INDEX `idx_ip_to` (`ip_to`)) ENGINE=MyISAM' 2>&1)"
-fi
+RESPONSE="$(mysql ip2proxy_database -e 'CREATE TABLE ip2proxy_database_tmp (`ip_from` DECIMAL(39,0) UNSIGNED NOT NULL,`ip_to` DECIMAL(39,0) UNSIGNED NOT NULL'"$FIELDS"',INDEX `idx_ip_to` (`ip_to`)) ENGINE=MyISAM' 2>&1)"
 
 [ ! -z "$(echo $RESPONSE)" ] && error "[ERROR]" || success "[OK]"
 
-echo -n " > [MySQL] Load CSV data into \"ip2proxy_database_tmp\" "
-
-RESPONSE="$(mysql ip2proxy_database -e 'LOAD DATA LOCAL INFILE '\'''$CSV''\'' INTO TABLE ip2proxy_database_tmp FIELDS TERMINATED BY '\'','\'' ENCLOSED BY '\''\"'\'' LINES TERMINATED BY '\''\n'\''' 2>&1)"
-
-[ ! -z "$(echo $RESPONSE)" ] && error "[ERROR]" || success "[OK]"
+for CSV in $(ls | grep '.CSV'); do
+	echo -n " > [MySQL] Load $CSV into database "
+	RESPONSE="$(mysql ip2proxy_database -e 'LOAD DATA LOCAL INFILE '\'''$CSV''\'' INTO TABLE ip2proxy_database_tmp FIELDS TERMINATED BY '\'','\'' ENCLOSED BY '\''\"'\'' LINES TERMINATED BY '\''\r\n'\''' 2>&1)"
+	[ ! -z "$(echo $RESPONSE)" ] && error "[ERROR]" || success "[OK]"
+done
 
 echo -n " > [MySQL] Drop table \"ip2proxy_database\" "
 
@@ -149,9 +188,10 @@ echo ""
 echo "   mysql -u admin -p$MYSQL_PASSWORD ip2proxy_database"
 echo ""
 
-echo "MYSQL_PASSWORD=$MYSQL_PASSWORD" > /config
-echo "TOKEN=$TOKEN" >> /config
-echo "CODE=$CODE" >> /config
+echo "MYSQL_PASSWORD=$MYSQL_PASSWORD" > /ip2proxy.conf
+echo "TOKEN=$TOKEN" >> /ip2proxy.conf
+echo "CODE=$CODE" >> /ip2proxy.conf
+echo "IP_TYPE=$IP_TYPE" >> /ip2proxy.conf
 
 rm -rf /_tmp
 
--- update.sh
+++ update.sh
@@ -3,13 +3,14 @@
 error() { echo -e "\e[91m$1\e[m"; exit 0; }
 success() { echo -e "\e[92m$1\e[m"; }
 
-if [ ! -f /config ]; then
+if [ ! -f /ip2proxy.conf ]; then
 	error "Missing configuration file."
 fi
 
-TOKEN=$(grep 'TOKEN' /config | cut -d= -f2)
-CODE=$(grep 'CODE' /config | cut -d= -f2)
-MYSQL_PASSWORD=$(grep 'MYSQL_PASSWORD' /config | cut -d= -f2)
+TOKEN=$(grep 'TOKEN' /ip2proxy.conf | cut -d= -f2)
+CODE=$(grep 'CODE' /ip2proxy.conf | cut -d= -f2)
+IP_TYPE=$(grep 'IP_TYPE' /ip2proxy.conf | cut -d= -f2)
+MYSQL_PASSWORD=$(grep 'MYSQL_PASSWORD' /ip2proxy.conf | cut -d= -f2)
 
 echo -n " > Create directory /_tmp "
 
@@ -21,25 +22,56 @@ cd /_tmp
 
 echo -n " > Download IP2Proxy database "
 
-wget -O database.zip -q --user-agent="Docker-IP2Proxy/MySQL" http://www.ip2location.com/download?token=${TOKEN}\&productcode=${CODE} > /dev/null 2>&1
+if [ "$IP_TYPE" == "IPV4" ]; then
+	wget -O ipv4.zip -q --user-agent="$USER_AGENT" "https://www.ip2location.com/download?token=${TOKEN}&code=${CODE}CSV" > /dev/null 2>&1
 
-[ ! -f database.zip ] && error "[DOWNLOAD FAILED]"
+	[ ! -z "$(grep 'NO PERMISSION' database.zip)" ] && error "[DENIED]"
+	[ ! -z "$(grep '5 TIMES' database.zip)" ] && error "[QUOTA EXCEEDED]"
 
-[ ! -z "$(grep 'NO PERMISSION' database.zip)" ] && error "[DENIED]"
+	RESULT=$(unzip -t ipv4.zip >/dev/null 2>&1)
 
-[ ! -z "$(grep '5 TIMES' database.zip)" ] && error "[QUOTA EXCEEDED]"
+	[ $? -ne 0 ] && error "[FILE CORRUPTED]"
+elif [ "$IP_TYPE" == "IPV6" ]; then
+	wget -O ipv6.zip -q --user-agent="$USER_AGENT" "https://www.ip2location.com/download?token=${TOKEN}&code=${CODE}CSVIPV6" > /dev/null 2>&1
 
-[ $(wc -c < database.zip) -lt 512000 ] && error "[FILE CORRUPTED]"
+	[ ! -z "$(grep 'NO PERMISSION' database.zip)" ] && error "[DENIED]"
+	[ ! -z "$(grep '5 TIMES' database.zip)" ] && error "[QUOTA EXCEEDED]"
+
+	RESULT=$(unzip -t ipv6.zip >/dev/null 2>&1)
+
+	[ $? -ne 0 ] && error "[FILE CORRUPTED]"
+else
+	wget -O ipv4.zip -q --user-agent="$USER_AGENT" "https://www.ip2location.com/download?token=${TOKEN}&code=${CODE}CSV" > /dev/null 2>&1
+	wget -O ipv6.zip -q --user-agent="$USER_AGENT" "https://www.ip2location.com/download?token=${TOKEN}&code=${CODE}CSVIPV6" > /dev/null 2>&1
+
+	[ ! -z "$(grep 'NO PERMISSION' ipv4.zip)" ] && error "[DENIED]"
+	[ ! -z "$(grep '5 TIMES' ipv4.zip)" ] && error "[QUOTA EXCEEDED]"
+
+	[ ! -z "$(grep 'NO PERMISSION' ipv6.zip)" ] && error "[DENIED]"
+	[ ! -z "$(grep '5 TIMES' ipv6.zip)" ] && error "[QUOTA EXCEEDED]"
+
+	RESULT=$(unzip -t ipv4.zip >/dev/null 2>&1)
+	[ $? -ne 0 ] && error "[FILE CORRUPTED]"
+
+	RESULT=$(unzip -t ipv6.zip >/dev/null 2>&1)
+	[ $? -ne 0 ] && error "[FILE CORRUPTED]"
+fi
 
 success "[OK]"
 
-echo -n " > Decompress downloaded package "
+for ZIP in $(ls | grep '.zip'); do
+	CSV=$(unzip -l $ZIP | grep '.CSV' | awk '{ print $4 }')
 
-unzip -q -o database.zip
+	echo -n " > Decompress $CSV from $ZIP "
 
-CSV="$(find . -name 'IP2PROXY*.CSV')"
+	unzip -jq $ZIP $CSV
 
-[ -z "$CSV" ] && error "[FILE CORRUPTED]" || success "[OK]"
+	if [ ! -f $CSV ]; then
+		error "[ERROR]"
+	fi
+
+	success "[OK]"
+done
 
 echo -n " > [MySQL] Create table \"ip2proxy_database_tmp\" "
 
@@ -47,11 +79,11 @@ RESPONSE="$(mysql ip2proxy_database -e 'DROP TABLE IF EXISTS ip2proxy_database_t
 
 [ ! -z "$(echo $RESPONSE)" ] && error "[ERROR]" || success "[OK]"
 
-echo -n " > [MySQL] Load CSV data into \"ip2proxy_database_tmp\" "
-
-RESPONSE="$(mysql ip2proxy_database -e 'LOAD DATA LOCAL INFILE '\'''$CSV''\'' INTO TABLE ip2proxy_database_tmp FIELDS TERMINATED BY '\'','\'' ENCLOSED BY '\''\"'\'' LINES TERMINATED BY '\''\r\n'\''' 2>&1)"
-
-[ ! -z "$(echo $RESPONSE)" ] && error "[ERROR]" || success "[OK]"
+for CSV in $(ls | grep '.CSV'); do
+	echo -n " > [MySQL] Load $CSV into database "
+	RESPONSE="$(mysql ip2proxy_database -e 'LOAD DATA LOCAL INFILE '\'''$CSV''\'' INTO TABLE ip2proxy_database_tmp FIELDS TERMINATED BY '\'','\'' ENCLOSED BY '\''\"'\'' LINES TERMINATED BY '\''\r\n'\''' 2>&1)"
+	[ ! -z "$(echo $RESPONSE)" ] && error "[ERROR]" || success "[OK]"
+done
 
 echo -n " > [MySQL] Rename table \"ip2proxy_database\" to \"ip2proxy_database_drop\" "
 
